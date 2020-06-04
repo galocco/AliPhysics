e37e92a6fe7d70d9b2ebbd79c9b624a5a75f889a
Giacomo Alocco <giacomo.alocco@edu.unito.it>
2020-06-04 10:00:00 +0200

    bugs fixed

--- PWGLF/NUCLEX/Hypernuclei/Hyp3Body/AddTaskHyperTritonVtxPerf.C
+++ PWGLF/NUCLEX/Hypernuclei/Hyp3Body/AddTaskHyperTritonVtxPerf.C
@@ -1,15 +1,3 @@
-AliAnaysisTaskHyperTriton3VtxPerf * AddTaskVtxPerf ( TString name = " name ") {
-  AliAnalysisManager * mgr = AliAnalysisManager::GetAnalysisManager();
-  TString fileName = AliAnalysisManager :: GetCommonFileName();
-  fileName += ": MyTask "; // create a subfolder in the file
-  // now we create an instance of your task
-  AliAnaysisTaskHyperTriton3VtxPerf * task = new AliAnaysisTaskHyperTriton3VtxPerf(true, name.Data());
-  // add your task to the manager
-  mgr->AddTask(task);
-  // connect the manager to your task
-  mgr->ConnectInput(task ,0 ,mgr->GetCommonInputContainer());
-  // same for the output
-  mgr->ConnectOutput(task ,1 ,mgr->CreateContainer("MyOutputContainer ", TList::Class (), AliAnalysisManager::kOutputContainer, fileName.Data())) ;
-  // important : return a pointer to your task
-  return task ;
+AliAnalysisTaskHyperTriton3VtxPerf *AddTaskHyperTritonVtxPerf(bool isMC = true, TString suffix = "") {
+  return AliAnalysisTaskHyperTriton3VtxPerf::AddTask(isMC, suffix);
 }
--- PWGLF/NUCLEX/Hypernuclei/Hyp3Body/runAnalysis.cpp
+++ PWGLF/NUCLEX/Hypernuclei/Hyp3Body/runAnalysis.cpp
@@ -1,8 +1,8 @@
-#include "AliAnalysisTaskHyperTriton3VtxPerf.h"
-#include "AddTaskHyperTritonVtxPerf.h"
+//#include "AliAnalysisTaskHyperTriton3VtxPerf.h"
+//#include "AddTaskHyperTritonVtxPerf.h"
 
 //#include "AliAnalysisManager.h"
-//#include "AliESDInputHandler.h"
+#include "AliESDInputHandler.h"
 
 void runAnalysis(){
   // header location
@@ -10,12 +10,17 @@ void runAnalysis(){
   gROOT->ProcessLine(".include $ALICE_ROOT/include");
   // create the analysis manager
   AliAnalysisManager* mgr = new AliAnalysisManager("AnalysisMyTask");
-  AliESDInputHandler* esdH = new AliESDInputHandler;
+  AliESDInputHandler* esdH = new AliESDInputHandler();
+  
   mgr->SetInputEventHandler(esdH);
   //???
   esdH->SetNeedField();
-  // compile the class ( locally )
+  // compile the class ( locally );
+  gROOT->ProcessLine(".L AliAnalysisTaskHyperTriton3VtxPerf.cxx+");
+  gROOT->ProcessLine(".L AliAnalysisTaskHyperTriton3VtxPerf.h+");
+  gROOT->ProcessLine(".L AddTaskHyperTritonVtxPerf.C+");
   gROOT->LoadMacro("AliAnalysisTaskHyperTriton3VtxPerf.cxx ++g") ;
+  gROOT->LoadMacro("AliAnalysisTaskHyperTriton3VtxPerf.h") ;
   // load the addtask macro and create the task
   AliAnalysisTaskHyperTriton3VtxPerf *task = reinterpret_cast<AliAnalysisTaskHyperTriton3VtxPerf*>(gInterpreter->ExecuteMacro("AddTaskHyperTritonVtxPerf.C"));
   // if you want to run locally , we need to define some input
@@ -23,4 +28,5 @@ void runAnalysis(){
   chain->Add("001/AliESDs.root");
   // start the analysis locally
   mgr->StartAnalysis("local", chain );
+  
 }
